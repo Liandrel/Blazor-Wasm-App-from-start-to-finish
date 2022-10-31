ff10ca2990985d7bb23aba79054cdda7ff5f2927
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-31 19:47:26 +0100

    Update Order
    Created update order functionality

--- BlazorWasmApp/Models/OrderUpdateModel.cs
+++ BlazorWasmApp/Models/OrderUpdateModel.cs
@@ -1,8 +1,16 @@
-﻿namespace BlazorWasmApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace BlazorWasmApp.Models
 {
     public class OrderUpdateModel
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20, ErrorMessage = "You need to keep the name to max of 20 characters")]
+        [MinLength(3, ErrorMessage = "You need to enter at least 3 characters for order name")]
+        [Display(Name = "Name for the Order")]
         public string OrderName { get; set; }
     }
 }
